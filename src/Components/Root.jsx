d50377f409e24cd0ab8e350c56caabf552964b9b
Muneeba Saleem <muneeba.centosquare@gmail.com>
2024-06-13 13:40:58 +0500

    Navbar designed

--- src/Components/Root.jsx
+++ src/Components/Root.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import App from "../App.jsx";
-import About from "./pages/About.jsx";
 import NavigationBar from "./NavigationBar.jsx";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
 
 export default function Root() {
     return (
-        <>
+        <Router>
             <NavigationBar></NavigationBar>
-            <App/>
-        </>
+            <div className="p-2 bg-blue-100">
+                <Routes>
+                    <Route path='/' element={<App/>}/>
+                    <Route path='/about' element={<About/>}/>
+                    <Route path='/contact' element={<Contact/>}/>
+                </Routes>
+            </div>
+        </Router>
     );
 }
