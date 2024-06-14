692e7d2289d57c6cb98ecf5c660fcaaf66e4341e
Muneeba Saleem <muneeba.centosquare@gmail.com>
2024-06-14 12:50:26 +0500

    bLOG Data

--- src/Components/pages/Reddit.jsx
+++ src/Components/pages/Reddit.jsx
@@ -3,14 +3,20 @@ import React, {useEffect, useState} from "react";
 export default function Reddit() {
     const [posts, setPosts] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [errorMessage, setErrorMessage] = useState(null);
 
     useEffect(() => {
         fetch('https://www.reddit.com/r/aww.json')
             .then(response => response.json())
             .then(results => {
                 setIsLoading(false);
+                console.log(results.data.children);
                 setPosts(results.data.children)
             })
+            .catch(error => {
+                setIsLoading(false);
+                setErrorMessage('There was an error');
+            });
     }, [])
 
     return (
@@ -28,13 +34,17 @@ export default function Reddit() {
                 <div className="grid grid-cols-4 gap-4">
                     {posts.map(post => (
                         <div className="border broder-gray-200">
-                            <img className="w-full" alt="something" src={post.data.thumbnail}/>
+                            <img className="w-full" alt={post.data.title || 'Reddit post'}
+                                 src={post.data.thumbnail}/>
                             <div className="p-2">
-                                <h6>{post.data.title}</h6>
+                                <h6 className="text-sm font-medium text-gray-700 leading-tight tracking-normal">{post.data.title}</h6>
                             </div>
                         </div>
                     ))}
                 </div>
+            )}
+            {errorMessage && (
+                <div>{errorMessage}</div>
             )}
         </>
     );
