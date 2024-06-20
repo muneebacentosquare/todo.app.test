ee8a384b4c4baebd5acb0d5c8be866f4e7b7aab0
Muneeba Saleem <muneeba.centosquare@gmail.com>
2024-06-20 12:53:46 +0500

    Product page design

--- src/Components/pages/Product.jsx
+++ src/Components/pages/Product.jsx
@@ -1,8 +1,196 @@
 import React from "react";
+
 export default function Product() {
     return (
-        <div className="w-2/5 p-5 my-10 mx-auto justify-center rounded shadow">
-            <h4>Product page</h4>
+        <div className="w-full lg:w-9/12 p-5 my-10 mx-auto justify-center rounded shadow">
+            <div className="grid grid-cols-3 gap-3">
+                <div className="border border-gray-200">
+                    <img alt="Images" height="200px" className="w-full"
+                         src="https://images.pexels.com/photos/1308881/pexels-photo-1308881.jpeg?auto=compress&cs=tinysrgb&w=600"/>
+                    <div className="p-2">
+                        <div className="flex justify-between items-center">
+                            <h6 className="font-normal text-lg text-black">Product heading</h6>
+                            <h6 className=" text-base font-semibold">
+                                $298
+                            </h6>
+                        </div>
+                        <p className="font-normal text-black text-sm">This is the description of product this is the
+                            description of product this is the description of product</p>
+                        <div className="flex justify-between items-center mt-2">
+                            <button
+                                className="py-1 px-2.5 rounded border border-gray-200 text-gray-800 bg-violet-300 text-base ">Add
+                                to Cart
+                            </button>
+                            <div className="flex space-x-1.5">
+                                <h6 className="text-sm">6</h6>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                     viewBox="0 0 24 24"
+                                     fill="#ffb638" stroke="#ffb638" strokeWidth="2"
+                                     strokeLinecap="round"
+                                     strokeLinejoin="round" className="lucide lucide-star">
+                                    <polygon
+                                        points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"/>
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="border border-gray-200">
+                    <img alt="Images" height="200px" className="w-full"
+                         src="https://images.pexels.com/photos/1308881/pexels-photo-1308881.jpeg?auto=compress&cs=tinysrgb&w=600"/>
+                    <div className="p-2">
+                        <div className="flex justify-between items-center">
+                            <h6 className="font-normal text-lg text-black">Product heading</h6>
+                            <h6 className=" text-base font-semibold">
+                                $298
+                            </h6>
+                        </div>
+                        <p className="font-normal text-black text-sm">This is the description of product this is the
+                            description of product this is the description of product</p>
+                        <div className="flex justify-between items-center mt-2">
+                            <button
+                                className="py-1 px-2.5 rounded border border-gray-200 text-gray-800 bg-violet-300 text-base ">Add
+                                to Cart
+                            </button>
+                            <div className="flex space-x-1.5">
+                                <h6 className="text-sm">6</h6>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                     viewBox="0 0 24 24"
+                                     fill="#ffb638" stroke="#ffb638" strokeWidth="2"
+                                     strokeLinecap="round"
+                                     strokeLinejoin="round" className="lucide lucide-star">
+                                    <polygon
+                                        points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"/>
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="border border-gray-200">
+                    <img alt="Images" height="200px" className="w-full"
+                         src="https://images.pexels.com/photos/1308881/pexels-photo-1308881.jpeg?auto=compress&cs=tinysrgb&w=600"/>
+                    <div className="p-2">
+                        <div className="flex justify-between items-center">
+                            <h6 className="font-normal text-lg text-black">Product heading</h6>
+                            <h6 className=" text-base font-semibold">
+                                $298
+                            </h6>
+                        </div>
+                        <p className="font-normal text-black text-sm">This is the description of product this is the
+                            description of product this is the description of product</p>
+                        <div className="flex justify-between items-center mt-2">
+                            <button
+                                className="py-1 px-2.5 rounded border border-gray-200 text-gray-800 bg-violet-300 text-base ">Add
+                                to Cart
+                            </button>
+                            <div className="flex space-x-1.5">
+                                <h6 className="text-sm">6</h6>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                     viewBox="0 0 24 24"
+                                     fill="#ffb638" stroke="#ffb638" strokeWidth="2"
+                                     strokeLinecap="round"
+                                     strokeLinejoin="round" className="lucide lucide-star">
+                                    <polygon
+                                        points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"/>
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="border border-gray-200">
+                    <img alt="Images" height="200px" className="w-full"
+                         src="https://images.pexels.com/photos/1308881/pexels-photo-1308881.jpeg?auto=compress&cs=tinysrgb&w=600"/>
+                    <div className="p-2">
+                        <div className="flex justify-between items-center">
+                            <h6 className="font-normal text-lg text-black">Product heading</h6>
+                            <h6 className=" text-base font-semibold">
+                                $298
+                            </h6>
+                        </div>
+                        <p className="font-normal text-black text-sm">This is the description of product this is the
+                            description of product this is the description of product</p>
+                        <div className="flex justify-between items-center mt-2">
+                            <button
+                                className="py-1 px-2.5 rounded border border-gray-200 text-gray-800 bg-violet-300 text-base ">Add
+                                to Cart
+                            </button>
+                            <div className="flex space-x-1.5">
+                                <h6 className="text-sm">6</h6>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                     viewBox="0 0 24 24"
+                                     fill="#ffb638" stroke="#ffb638" strokeWidth="2"
+                                     strokeLinecap="round"
+                                     strokeLinejoin="round" className="lucide lucide-star">
+                                    <polygon
+                                        points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"/>
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="border border-gray-200">
+                    <img alt="Images" height="200px" className="w-full"
+                         src="https://images.pexels.com/photos/1308881/pexels-photo-1308881.jpeg?auto=compress&cs=tinysrgb&w=600"/>
+                    <div className="p-2">
+                        <div className="flex justify-between items-center">
+                            <h6 className="font-normal text-lg text-black">Product heading</h6>
+                            <h6 className=" text-base font-semibold">
+                                $298
+                            </h6>
+                        </div>
+                        <p className="font-normal text-black text-sm">This is the description of product this is the
+                            description of product this is the description of product</p>
+                        <div className="flex justify-between items-center mt-2">
+                            <button
+                                className="py-1 px-2.5 rounded border border-gray-200 text-gray-800 bg-violet-300 text-base ">Add
+                                to Cart
+                            </button>
+                            <div className="flex space-x-1.5">
+                                <h6 className="text-sm">6</h6>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                     viewBox="0 0 24 24"
+                                     fill="#ffb638" stroke="#ffb638" strokeWidth="2"
+                                     strokeLinecap="round"
+                                     strokeLinejoin="round" className="lucide lucide-star">
+                                    <polygon
+                                        points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"/>
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="border border-gray-200">
+                    <img alt="Images" height="200px" className="w-full"
+                         src="https://images.pexels.com/photos/1308881/pexels-photo-1308881.jpeg?auto=compress&cs=tinysrgb&w=600"/>
+                    <div className="p-2">
+                        <div className="flex justify-between items-center">
+                            <h6 className="font-normal text-lg text-black">Product heading</h6>
+                            <h6 className=" text-base font-semibold">
+                                $298
+                            </h6>
+                        </div>
+                        <p className="font-normal text-black text-sm">This is the description of product this is the
+                            description of product this is the description of product</p>
+                        <div className="flex justify-between items-center mt-2">
+                            <button
+                                className="py-1 px-2.5 rounded border border-gray-200 text-gray-800 bg-violet-300 text-base ">Add
+                                to Cart
+                            </button>
+                            <div className="flex space-x-1.5">
+                                <h6 className="text-sm">6</h6>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                     viewBox="0 0 24 24"
+                                     fill="#ffb638" stroke="#ffb638" strokeWidth="2"
+                                     strokeLinecap="round"
+                                     strokeLinejoin="round" className="lucide lucide-star">
+                                    <polygon
+                                        points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"/>
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
