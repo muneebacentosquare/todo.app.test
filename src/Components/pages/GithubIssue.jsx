b5a2a67041b021e5964bd02c00237e8551ef074c
Muneeba Saleem <muneeba.centosquare@gmail.com>
2024-07-25 16:00:43 +0500

    Github issues task

---
+++ src/Components/pages/GithubIssue.jsx
@@ -0,0 +1,154 @@
+import React from "react";
+
+export default function GithubIssue() {
+    return (
+        <div className="w-9/12 my-10 mx-auto justify-center">
+            <div className="grid grid-cols-1 divide-y rounded shadow">
+                <div className="bg-gray-100 rounded-t p-5">
+                    <a href="#" className="text-xl text-blue-700 font-medium">
+                        Facebook/create-react-app
+                    </a>
+                    <div className="flex space-x-3 items-center mt-0.5">
+                        <div className="flex items-center space-x-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+                                 fill="none"
+                                 stroke="#1b9d42" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                                 className="lucide lucide-circle-dot">
+                                <circle cx="12" cy="12" r="10"/>
+                                <circle cx="12" cy="12" r="1"/>
+                            </svg>
+                            <div className="text-sm font-semibold">
+                                96 Open
+                            </div>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+                                 fill="none"
+                                 stroke="#a02c2c" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                                 className="lucide lucide-circle-check">
+                                <circle cx="12" cy="12" r="10"/>
+                                <path d="m9 12 2 2 4-4"/>
+                            </svg>
+                            <div className="text-sm font-semibold">
+                                96 Open
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex items-center justify-between px-5 py-3">
+                    <div className="flex items-center space-x-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+                             stroke="#1b9d42" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                             className="lucide lucide-circle-dot">
+                            <circle cx="12" cy="12" r="10"/>
+                            <circle cx="12" cy="12" r="1"/>
+                        </svg>
+                        <div>
+                            <a href="#" className="text-base text-black font-semibold">
+                                Issue with running single test with jest command
+                            </a>
+                            <p className="text-gray-600 font-medium text-xs">#111567 opened 10 hours ago by
+                                someIdiot</p>
+                        </div>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 24 24" fill="none"
+                             stroke="#1f2937" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                             className="lucide lucide-message-square">
+                            <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>
+                        </svg>
+                        <div className="text-sm font-semibold">
+                            123
+                        </div>
+                    </div>
+                </div>
+                <div className="flex items-center justify-between px-5 py-3">
+                    <div className="flex items-center space-x-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+                             stroke="#1b9d42" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                             className="lucide lucide-circle-dot">
+                            <circle cx="12" cy="12" r="10"/>
+                            <circle cx="12" cy="12" r="1"/>
+                        </svg>
+                        <div>
+                            <a href="#" className="text-base text-black font-semibold">
+                                Issue with running single test with jest command
+                            </a>
+                            <p className="text-gray-600 font-medium text-xs">#111567 opened 10 hours ago by
+                                someIdiot</p>
+                        </div>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 24 24" fill="none"
+                             stroke="#1f2937" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                             className="lucide lucide-message-square">
+                            <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>
+                        </svg>
+                        <div className="text-sm font-semibold">
+                            123
+                        </div>
+                    </div>
+                </div>
+                <div className="flex items-center justify-between px-5 py-3">
+                    <div className="flex items-center space-x-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+                             stroke="#1b9d42" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                             className="lucide lucide-circle-dot">
+                            <circle cx="12" cy="12" r="10"/>
+                            <circle cx="12" cy="12" r="1"/>
+                        </svg>
+                        <div>
+                            <a href="#" className="text-base text-black font-semibold">
+                                Issue with running single test with jest command
+                            </a>
+                            <p className="text-gray-600 font-medium text-xs">#111567 opened 10 hours ago by
+                                someIdiot</p>
+                        </div>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 24 24" fill="none"
+                             stroke="#1f2937" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                             className="lucide lucide-message-square">
+                            <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>
+                        </svg>
+                        <div className="text-sm font-semibold">
+                            123
+                        </div>
+                    </div>
+                </div>
+                <div className="flex items-center justify-between px-5 py-3">
+                    <div className="flex items-center space-x-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+                             stroke="#1b9d42" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                             className="lucide lucide-circle-dot">
+                            <circle cx="12" cy="12" r="10"/>
+                            <circle cx="12" cy="12" r="1"/>
+                        </svg>
+                        <div>
+                            <a href="#" className="text-base text-black font-semibold">
+                                Issue with running single test with jest command
+                            </a>
+                            <p className="text-gray-600 font-medium text-xs">#111567 opened 10 hours ago by
+                                someIdiot</p>
+                        </div>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 24 24" fill="none"
+                             stroke="#1f2937" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                             className="lucide lucide-message-square">
+                            <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>
+                        </svg>
+                        <div className="text-sm font-semibold">
+                            123
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <p className="text-xl text-black font-medium mt-5">
+                [DevTools] Improve named hooks detection <span className="font-normal">#93294</span>
+            </p>
+            <p className="text-gray-600 font-medium text-xs"><span className="font-semibold">111567</span> opened 10 hours ago by
+                someIdiot</p>
+        </div>
+    );
+}
